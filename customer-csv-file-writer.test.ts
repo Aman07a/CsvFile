72e497cec60358f09fb8a911daa81bcc34b62693
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-27 17:19:52 +0200

    Worked Example: CSV File Requirement 2.1

--- customer-csv-file-writer.test.ts
+++ customer-csv-file-writer.test.ts
@@ -10,66 +10,133 @@ import { CustomerCsvFileWriter } from "./customer-csv-file-writer";
 import { FileWriter } from "./file-writer";
 
 describe("CustomerCsvFileWriter", () => {
-  describe("null customers array", () => {
-    test("should throw an argument exception", () => {
-      // Arrange
-      const fileWriter = createFileWriter();
-      const sut = createCustomerCsvFileWriter(fileWriter);
-      const fileName = "customers.csv";
-      // Act/Assert
-      expect(() => sut.writeCustomers(fileName, null!)).toThrowError(
-        "customers"
-      );
+  describe("writeCustomers", () => {
+    describe("null customers array", () => {
+      test("should throw an argument exception", () => {
+        // Arrange
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        const fileName = "customers.csv";
+        // Act/Assert
+        expect(() => sut.writeCustomers(fileName, null!)).toThrowError(
+          "customers"
+        );
+      });
+    });
+
+    describe("no customer", () => {
+      test("should not write any lines", () => {
+        // Arrange
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        const fileName = "customers.csv";
+        const customers: Customer[] = [];
+        // Act
+        sut.writeCustomers(fileName, customers);
+        // Assert
+        assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
+      });
+    });
+
+    describe("one customer", () => {
+      test.each([
+        { customer: createCustomer("Peter Wiles", "12345697123") },
+        { customer: createCustomer("Brendon Page", "45648484654") },
+      ])("customer: $customer", ({ customer }) => {
+        // Arrange
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        const fileName = "customers.csv";
+        const customers = [customer];
+        // Act
+        sut.writeCustomers(fileName, customers);
+        // Assert
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(1);
+        assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
+      });
+    });
+
+    describe("many customers", () => {
+      test("should write all customers", () => {
+        // Arrange
+        const customers = [
+          createCustomer("Peter Wiles", "12345697123"),
+          createCustomer("Brendon Page", "45648484654"),
+          createCustomer("Bob", "97987987987"),
+        ];
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        const fileName = "cust.csv";
+        // Act
+        sut.writeCustomers(fileName, customers);
+        // Assert
+        assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
+      });
     });
   });
 
-  describe("no customer", () => {
-    test("should not write any lines", () => {
-      // Arrange
-      const fileWriter = createFileWriter();
-      const sut = createCustomerCsvFileWriter(fileWriter);
-      const fileName = "customers.csv";
-      const customers: Customer[] = [];
-      // Act
-      sut.writeCustomers(fileName, customers);
-      // Assert
-      assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+  describe("writeCustomersBatched", () => {
+    describe("less than 10 customers", () => {
+      test("should not batch in one group", () => {
+        // Arrange
+        const customers = [
+          createCustomer("a", "1"),
+          createCustomer("b", "2"),
+          createCustomer("c", "3"),
+          createCustomer("d", "4"),
+          createCustomer("e", "5"),
+          createCustomer("f", "6"),
+          createCustomer("g", "7"),
+          createCustomer("h", "8"),
+        ];
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        const fileName = "batchedcust.csv";
+        // Act
+        sut.writeCustomersBatched(fileName, customers);
+        // Assert
+        assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
+      });
     });
-  });
 
-  describe("one customer", () => {
-    test.each([
-      { customer: createCustomer("Peter Wiles", "12345697123") },
-      { customer: createCustomer("Brendon Page", "45648484654") },
-    ])("customer: $customer", ({ customer }) => {
-      // Arrange
-      const fileWriter = createFileWriter();
-      const sut = createCustomerCsvFileWriter(fileWriter);
-      const fileName = "customers.csv";
-      const customers = [customer];
-      // Act
-      sut.writeCustomers(fileName, customers);
-      // Assert
-      expect(fileWriter.writeLine).toHaveBeenCalledTimes(1);
-      assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
-    });
-  });
-
-  describe("many customers", () => {
-    test("should write all customers", () => {
-      // Arrange
-      const customers = [
-        createCustomer("Peter Wiles", "12345697123"),
-        createCustomer("Brendon Page", "45648484654"),
-        createCustomer("Bob", "97987987987"),
-      ];
-      const fileWriter = createFileWriter();
-      const sut = createCustomerCsvFileWriter(fileWriter);
-      const fileName = "cust.csv";
-      // Act
-      sut.writeCustomers(fileName, customers);
-      // Assert
-      assertCustomerWereWrittenToFile(fileWriter, fileName, customers);
+    describe("more than 10 customers", () => {
+      test("should not batch in groups of 10", () => {
+        // Arrange
+        const customers = [
+          createCustomer("a", "1"),
+          createCustomer("b", "2"),
+          createCustomer("c", "3"),
+          createCustomer("d", "4"),
+          createCustomer("e", "5"),
+          createCustomer("f", "6"),
+          createCustomer("g", "7"),
+          createCustomer("h", "8"),
+          createCustomer("i", "9"),
+          createCustomer("j", "10"),
+          createCustomer("k", "11"),
+          createCustomer("l", "12"),
+        ];
+        const fileWriter = createFileWriter();
+        const sut = createCustomerCsvFileWriter(fileWriter);
+        // Act
+        sut.writeCustomersBatched("batchedcust.csv", customers);
+        // Assert
+        assertCustomerWereWrittenToFile(
+          fileWriter,
+          "batchedcust1.csv",
+          customers.slice(0, 10)
+        );
+        assertCustomerWereWrittenToFile(
+          fileWriter,
+          "batchedcust2.csv",
+          customers.slice(10, 12)
+        );
+        expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
+      });
     });
   });
 });
@@ -82,7 +149,6 @@ function assertCustomerWereWrittenToFile(
   customers.forEach((customer) => {
     assertCustomerWasWrittenToFile(fileWriter, fileName, customer);
   });
-  expect(fileWriter.writeLine).toHaveBeenCalledTimes(customers.length);
 }
 
 function assertCustomerWasWrittenToFile(
--- customer-csv-file-writer.ts
+++ customer-csv-file-writer.ts
@@ -13,6 +13,15 @@ export class CustomerCsvFileWriter {
     });
   }
 
+  writeCustomersBatched(fileName: string, customers: Customer[]) {
+    if (customers.length > 10) {
+      this.writeCustomers("batchedcust1.csv", customers.slice(0, 10));
+      this.writeCustomers("batchedcust2.csv", customers.slice(10, 12));
+    } else {
+      this.writeCustomers(fileName, customers);
+    }
+  }
+
   private formatAsCsvRow(customer: Customer): string {
     return `${customer.name},${customer.contactNumber}`;
   }
